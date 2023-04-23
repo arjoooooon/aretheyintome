ebe85d5bae54a67b7103956af7f480037112f2c5
Kerria Pang-Naylor <kerria.lyona11235@gmail.com>
2023-04-23 05:06:56 -0700

    added gpt stuff

--- backend/cohere_classifier.py
+++ backend/cohere_classifier.py
@@ -13,6 +13,7 @@ co = cohere.Client(api_key)
 
 # OPENAI
 openai.api_key_path = './.env'
+prompt_system = "You are a dating coach talking to a romantically unsuccessful college CS major (likely male) who is confused by their crush's text messages."
 
 # HUGGING FACE
 
@@ -65,21 +66,28 @@ def generate_text(user_prompt):
     return response.generations[0].text
 
 def chatgpt(prompt):
-    response = openai.Completion.create(
-        model = "text-davinci-003",
-        prompt = prompt,
-        temperature = 0.6,
-    )
-    return response
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+        messages=[
+        {"role": "system", "content": prompt_system},
+        {"role": "user", "content": prompt}
+        # {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        # {"role": "user", "content": "Where was it played?"}
+    ])
+    return response['choices'][0]['message']["content"]
+
+    # response = openai.Completion.create(
+    #     model = "text-davinci-003",
+    #     prompt = prompt,
+    #     temperature = 0.6,
+    # )
+    # return response
 
 def but_why(responses, inputs):
     """List of inputs and cohere's responses, cohere will explain why for each one"""
-    #pdb.set_trace()
     prompt = ""
     for i, response in enumerate(responses):
-        prompt = prompt + str(i+1) + f". Explain why this text conversation was classified as \"{response.prediction}\" with {round(response.confidence,3)} confidence: {inputs[i]}."
-    #prompt += "\n This is an example answer you can use: \'This conversation was classified as \"friendzone\" with a 0.977 confidence. This means that the conversation was clearly not flirtatious and indicated that the person was not interested in a romantic relationship. The phrase \"Hey I think you\'re really cute btw :)\" is a clear expression of interest, but the response \"aww you\'re such a good friend\" indicates that the other person sees the sender only as a friend and is not interested in taking things further.\'"
-    print(prompt)
+        prompt += str(i+1) + f". Explain why this text conversation was classified as \"{response.prediction}\" with {round(response.confidence,3)} confidence: \"{inputs[i]}\".\n"
+    #print(prompt)
     return chatgpt(prompt)
 
 if __name__ == "__main__":
@@ -89,7 +97,6 @@ if __name__ == "__main__":
     # response = train_classification(train_df, inputs, text_col = "conversations", label_col="label")
     #inputs = ["Hi, wanna go to the beach?\n oh no sorry, I can't\n hey how are you doing?", "Hey I think you're really cute btw :)\n aww you such a good friend"]
     #inputs = ["I love you baby\n oh I love you too <3", "Hey what are you up to?\n Not much, just chillin\n Wanna go get drinks? \n Sure!"]#["Hi sweetie? \n Hi babe, what are you up to?", "Hey, wanna go out?\n sure, but can my friend come?"]
-    print("bruh")
     inputs = ["Hey, I\'m Alan Wu, a fanatical explorer of { AI, Data, Algo, Quant, Crypto, Generative Art, Competitions, ... }. Currently studying CS+Math @ Harvey Mudd College as a sophomore.\n\nI love to meet and compete with the brightest minds. I\'m active in { Programming, Math, Data Science } competitions. Check out my progress in Blog and Projects.\n\nThis summer, I\'m joining Jane Street as a Quantitative Trading Intern. Currently, I\'m working at Crabel Asset Management part-time as a Quantitative Research Intern.\n\nI\'m very optimistic about the future of AI. While learning more, I did some research on computer vision and autonomous driving. View my publication on IEEE Xplore."]
     responses = classify_friendzone(inputs)
 
